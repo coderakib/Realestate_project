2ebf21447d5b9249ab8cea177193d0f6a8481e6d
Akib Hussain <coder.akib0903@gmail.com>
2021-12-25 11:51:58 +0530

    first commit

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -100,7 +100,7 @@ export default function SearchFilters() {
                 ))}
                 {!loading && !locationData?.length && (
                   <Flex justifyContent='center' alignItems='center' flexDir='column' marginTop='5' marginBottom='5' >
-                    <Image src={noresult} />
+                    <Image src={noresult} alt="description of image" />
                     <Text fontSize='xl' marginTop='3'>
                       Waiting to search!
                     </Text>
